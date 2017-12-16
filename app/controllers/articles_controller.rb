f7a9b0504332b45307ee5c70843f1962e64fcac2
pelamx <pelamx@gmail.com>
2017-12-16 05:06:22 +0300

    Show action ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,24 @@
 class ArticlesController < ApplicationController
   def new
-    @articles = Article.new
+    @article = Article.new
   end
+
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
     def article_params
-      params.require(:articles).permit(:title, :description)
+      params.require(:article).permit(:title, :description)
     end
 end
