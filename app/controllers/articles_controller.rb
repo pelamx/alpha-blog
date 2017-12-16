bba0817d0f58fc032eaa3c3166c0d6105107f5af
pelamx <pelamx@gmail.com>
2017-12-16 03:50:07 +0300

    Create new template, action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  def new
+    @articles = Article.new
+  end
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  private
+    def article_params
+      params.require(:articles).permit(:title, :description)
+    end
+end
