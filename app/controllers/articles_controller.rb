cb3e47df8ea829ef3dce8d311f6d88c310a279c2
pelamx <pelamx@gmail.com>
2017-12-19 04:04:39 +0300

    edit, update functions done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,25 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -15,6 +34,16 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    if @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+      redirect_to article_path(@article)
+    end
   end
 
   private
