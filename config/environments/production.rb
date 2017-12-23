d9d466915afc2ee44761f80df485e8eee3f71f45
pelamx <pelamx@gmail.com>
2017-12-23 05:37:27 +0300

    last

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
