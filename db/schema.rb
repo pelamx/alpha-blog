9038f83dcb0738570d1b19cdc1450cf5d521c100
pelamx <pelamx@gmail.com>
2018-01-12 19:31:48 +0300

    associations between user and articles and debugger view on pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213113355) do
+ActiveRecord::Schema.define(version: 20180112102330) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title" # after rake db:migrate it adds automaticcally here from the file create_articles.rb
+    t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
